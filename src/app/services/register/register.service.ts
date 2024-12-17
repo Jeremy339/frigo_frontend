cd48c0e7674f8ff60a5585d931fe5ff9964c6428
Jeremy339 <jal.pachoqyavirac.edu.ec>
2024-12-16 21:31:56 -0500

    Register

--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private apiUrl = 'http://localhost:3000/usuarios'; // La URL de tu API para crear usuarios
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Método para crear un usuario
+  createUser(user: any): Observable<any> {
+    return this.http.post(this.apiUrl, user);
+  }
 }
